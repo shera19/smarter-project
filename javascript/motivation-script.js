b6e7c701539ac69bef6caaf90fd63abc8141f8b9
shera19 <aybekov.shera@gmail.com>
2020-11-19 11:53:36 -0800

    Finished video page

--- javascript/motivation-script.js
+++ javascript/motivation-script.js
@@ -5,14 +5,12 @@ const twitterBtn=document.getElementById('twitter');
 const newQuoteBtn=document.getElementById('new-quote');
 const loader=document.getElementById('loader');
 
-// Show Loading
- function loading() {
+ function showLoadingSpinner() {
      loader.hidden=false;
      quoteContainer.hidden=true;
  }
 
-//  Hide Loading
-function complete() {
+function hideLoadingSpinner() {
     if(!loader.hidden){
         quoteContainer.hidden=false;
         loader.hidden=true;
@@ -21,7 +19,7 @@ function complete() {
 
 // Get Quote From API
 async function getQuote() {
-    loading();
+    showLoadingSpinner();
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
     const apiUrl='http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
@@ -41,8 +39,7 @@ async function getQuote() {
             quoteText.classList.remove('long-quote');
         }
         quoteText.innerText=data.quoteText;
-        // Stop Loader, Show Quote
-        complete();
+        hideLoadingSpinner();
     } catch (error) {
         getQuote();
     }
